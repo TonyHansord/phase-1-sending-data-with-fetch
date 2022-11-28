620792da4303896607bdc00a42df79ae7f10a1c4
Anthony Hansord <tonyhansorddev@gmail.com>
2022-11-28 20:43:04 +1100

    commit tree 1cdfbfb6a585802c8782d9675fe72b4a636dfdf2

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+const submitData = (name, email) => {
+  return fetch(`http://localhost:3000/users`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email,
+    }),
+  })
+    .then((response) => response.json())
+    .then((obj) => {
+      let id = document.createElement('p');
+      id.textContent = `ID: ${obj.id}`;
+      document.body.appendChild(id);
+    })
+    .catch((err) => {
+      let errorMessage = document.createElement('p');
+      errorMessage.textContent = err.message;
+      document.body.appendChild(errorMessage);
+    });
+};
